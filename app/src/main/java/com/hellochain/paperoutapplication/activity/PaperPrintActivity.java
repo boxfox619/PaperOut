cbac09c8f5d9afde7151f92dc19ce28fac4e4c2e
rlatjdfo112 <rlatjdfo112@naver.com>
2017-08-29 23:07:40 +0900

    Finish develop the fingerprint authentication function

--- app/src/main/java/com/hellochain/paperoutapplication/activity/PaperPrintActivity.java
+++ app/src/main/java/com/hellochain/paperoutapplication/activity/PaperPrintActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.ajalt.reprint.core.Reprint;
 import com.hellochain.paperoutapplication.R;
 import com.hellochain.paperoutapplication.view.tablayout.FingerprintDetectionPage;
 import com.hellochain.paperoutapplication.view.tablayout.PaperPrintFinishFragment;
@@ -25,6 +26,7 @@ public class PaperPrintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paper_print);
+        Reprint.initialize(this);
 
 
         this.pageHandler = new PageHandler();
--- app/src/main/java/com/hellochain/paperoutapplication/view/tablayout/FingerprintDetectionPage.java
+++ app/src/main/java/com/hellochain/paperoutapplication/view/tablayout/FingerprintDetectionPage.java
@@ -1,6 +1,7 @@
 package com.hellochain.paperoutapplication.view.tablayout;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -10,13 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.androidquery.AQuery;
-import com.androidquery.callback.AjaxCallback;
-import com.androidquery.callback.AjaxStatus;
+import com.github.ajalt.reprint.core.AuthenticationFailureReason;
+import com.github.ajalt.reprint.core.AuthenticationListener;
+import com.github.ajalt.reprint.core.Reprint;
 import com.hellochain.paperoutapplication.R;
 
 import org.json.JSONArray;
@@ -26,8 +27,12 @@ import org.json.JSONArray;
  */
 
 public class FingerprintDetectionPage extends Fragment {
+    private static final int MAX_ERROR_COUNT = 5;
     private Handler handler;
     private int num;
+
+    private int count;
+    private TextView guideTextView;
 
     public static FingerprintDetectionPage getInstance(int num, Handler handler) {
         FingerprintDetectionPage fragment = new FingerprintDetectionPage();
@@ -41,7 +46,51 @@ public class FingerprintDetectionPage extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_fingerprint_dection, container, false);
         view.findViewById(R.id.enter_password_direct).setOnClickListener((v) -> showPasswordInputDialog());
+        guideTextView = (TextView) view.findViewById(R.id.tv_guide);
+        Reprint.authenticate(new AuthenticationListener() {
+            public void onSuccess(int moduleTag) {
+                sendResultMsg(true);
+            }
+
+            public void onFailure(AuthenticationFailureReason failureReason, boolean fatal,
+                                  CharSequence errorMessage, int moduleTag, int errorCode) {
+                if (failureReason.equals(AuthenticationFailureReason.LOCKED_OUT)) return;
+                if (failureReason.equals(AuthenticationFailureReason.AUTHENTICATION_FAILED))
+                    if (count == MAX_ERROR_COUNT) {
+                        sendResultMsg(false);
+                    } else {
+                        showErrorMsg();
+                    }
+                else {
+                    setFingerPrintUnable();
+                }
+            }
+        });
         return view;
+    }
+
+    private void setFingerPrintUnable() {
+        guideTextView.setText(getResources().getString(R.string.error_unable_fingerprint));
+    }
+
+    private void sendResultMsg(boolean result) {
+        Message msg = handler.obtainMessage();
+        msg.what = num;
+        msg.obj = result;
+        handler.sendMessage(msg);
+    }
+
+    private void showErrorMsg() {
+        count += 1;
+        guideTextView.setText(getResources().getString(R.string.error_retry_fingerprint));
+        guideTextView.setTextColor(Color.RED);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                guideTextView.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        }, 1000);
     }
 
     private void showPasswordInputDialog() {
