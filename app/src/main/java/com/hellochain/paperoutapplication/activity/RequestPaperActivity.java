408d4b45f77b79263b9a7a42b06f3292ab8082df
rlatjdfo112 <rlatjdfo112@naver.com>
2017-08-29 12:26:00 +0900

    Implement the Paper request activity

---
+++ app/src/main/java/com/hellochain/paperoutapplication/activity/RequestPaperActivity.java
@@ -0,0 +1,15 @@
+package com.hellochain.paperoutapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hellochain.paperoutapplication.R;
+
+public class RequestPaperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_paper);
+    }
+}
--- app/src/main/java/com/hellochain/paperoutapplication/activity/SplashActivity.java
+++ app/src/main/java/com/hellochain/paperoutapplication/activity/SplashActivity.java
@@ -18,6 +18,6 @@ public class SplashActivity extends AppCompatActivity {
 
         Realm.init(this);
 
-        startActivity(new Intent(this, SignupActivity.class));
+        startActivity(new Intent(this, RequestPaperActivity.class));
     }
 }
